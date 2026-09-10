08800cf11715650f39b1e4845a2ed883b83820a8
Mustafa TAVASLI <326676217+tavasli@users.noreply.github.com>
2026-09-10 15:29:18 +0300

    feat(script): create invitation card

---
+++ script.py
@@ -0,0 +1,16 @@
+host = "Mustafa"
+date = "September 10"
+dress_code = "Casual"
+name = "Sude"
+
+print("=" * 40)
+print()
+print(f" {name}, you are cordially invited to:")
+print()
+print("\tLearn Python")
+print()
+print(f"Hosted by: {host}")
+print(f"Date: {date}")
+print(f"Dress Code: {dress_code}")
+print()
+print("=" * 40)

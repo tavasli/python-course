1b3e9e1f7624f0e00c0a641a696f140886c0c287
Mustafa TAVASLI <326676217+tavasli@users.noreply.github.com>
2026-09-10 15:56:17 +0300

    refactor(script): work with variables

--- script.py
+++ script.py
@@ -1,16 +1,28 @@
-host = "Mustafa"
-date = "September 10"
-dress_code = "Casual"
-name = "Sude"
+my_party = 2
+print(my_party)
 
-print("=" * 40)
-print()
-print(f" {name}, you are cordially invited to:")
-print()
-print("\tLearn Python")
-print()
-print(f"Hosted by: {host}")
-print(f"Date: {date}")
-print(f"Dress Code: {dress_code}")
-print()
-print("=" * 40)
+my_party += 2
+print(my_party)
+
+food_total = 100
+drinks_total = 15
+meal_total = food_total + drinks_total
+print(meal_total)
+# drinks_total -= 5
+# Güncelleme olsa da meal_total değişkeni etkilenmez. O yüzden direkt gidip değiştirelim.
+print(meal_total)
+
+morning = "Drink coffee"
+afternoon = "Skiing"
+evening = "Watching a movie"
+plan_for_today = "My plan for today is: 1. " + morning + " 2. " + afternoon + " 3. " + evening
+print(plan_for_today)
+
+first_name = "Metin"
+event = "football match"
+number = 2
+noun = "meetings"
+verb = "attend"
+excuse = f"Sorry {first_name}, I can't come to the {event} because I have a {number} {noun} to {verb}."
+# f-string ile direkt değeri alabiliyoruz.
+print(excuse)
